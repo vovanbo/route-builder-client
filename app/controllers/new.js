09e11be72fbbaa2ebb3b1229ff5d74db6637ada3
Vladimir Bolshakov <vovanbo@gmail.com>
2016-06-29 20:35:16 +0300

    Properly errors handling in route's creation form.

--- app/controllers/new.js
+++ app/controllers/new.js
@@ -1,37 +1,69 @@
 import Ember from 'ember';
-// import { polyline } from 'bower_components/polyline/src/polyline.js';
 
 export default Ember.Controller.extend({
   origin: 'Санкт-Петербург',
   destination: 'Москва',
 
   actions: {
+    resetForm() {
+      this.set('isReadyToCreate', false);
+      this.set('formErrors', undefined);
+      this.set('routePreview', undefined);
+    },
+
     createRoute() {
       let origin = this.get('origin');
       let destination = this.get('destination');
-      Ember.Logger.info(origin, destination);
-      this.store.query('direction', { origin: origin, destination: destination }).then((directions) => {
-        return directions.get('firstObject');
-      }).then((direction) => {
-        let route = direction.get('route');
-        let leg = route.legs[0];
-        let routePreview = Ember.Object.create({
-          "origin": {
-            "type": "Point",
-            "coordinates": [leg.start_location.lng, leg.start_location.lat]
-          },
-          "originName": leg.start_address,
-          "destination": {
-            "type": "Point",
-            "coordinates": [leg.end_location.lng, leg.end_location.lat]
-          },
-          "destinationName": leg.end_address,
-          "bounds": route.bounds,
-          "polyline": polyline.toGeoJSON(route.overview_polyline.points)
+      let isReadyToCreate = this.get('isReadyToCreate');
+      if (isReadyToCreate) {
+        let routePreview = this.get('routePreview');
+        let routeProperties = routePreview.getProperties(
+          'origin', 'originName', 'destination', 'destinationName',
+          'bounds', 'polyline'
+        );
+        let newRoute = this.store.createRecord('route', routeProperties);
+        newRoute.save().then((result) => {
+          newRoute.set('id', result.get('id'));
+          newRoute.set('created', result.get('created'));
+          this.transitionToRoute('routes.show', newRoute.get('id'));
         });
-        Ember.Logger.info(routePreview);
-        this.set('routePreview', routePreview);
-      });
+      }
+      else {
+        this.set('isLoading', true);
+        this.store.query('direction', { origin: origin, destination: destination })
+          .then((directions) => {
+            return directions.get('firstObject');
+          })
+          .then((direction) => {
+            this.set('isLoading', false);
+            if (direction) {
+              let route = direction.get('route');
+              let leg = route.legs[0];
+              let routePreview = Ember.Object.create({
+                "origin": {
+                  "type": "Point",
+                  "coordinates": [leg.start_location.lng, leg.start_location.lat]
+                },
+                "originName": leg.start_address,
+                "destination": {
+                  "type": "Point",
+                  "coordinates": [leg.end_location.lng, leg.end_location.lat]
+                },
+                "destinationName": leg.end_address,
+                "bounds": route.bounds,
+                "polyline": polyline.toGeoJSON(route.overview_polyline.points)
+              });
+              this.set('isReadyToCreate', true);
+              this.set('routePreview', routePreview);
+            }
+            else {
+              // this.set('isFormHasErrors', true);
+              this.set('formErrors', ['Route cannot be builded for passed points.'])
+            }
+          }).catch((error) => {
+            Ember.Logger.info(error);
+          });
+      }
     }
   }
 });
--- app/routes/new.js
+++ app/routes/new.js
@@ -4,7 +4,16 @@ export default Ember.Route.extend({
   actions: {
     didTransition() {
       this.controller.set('routePreview', undefined);
+      this.controller.set('isReadyToCreate', false);
+      this.controller.set('formErrors', undefined);
       return true;
+    },
+
+    loading(transition, originRoute) {
+      this.controller.set('isLoading', true);
+      transition.promise.finally(function() {
+          this.controller.set('isLoading', false);
+      });
     }
   }
 });
